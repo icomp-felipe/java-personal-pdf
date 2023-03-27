bfadd935d38ae898d0fcb87da4fb38f2f3b4d27a
Felipe André <fass@icomp.ufam.edu.br>
2023-03-27 19:13:27 -0400

    First commit

---
+++ PDF Exporters/src/com/pdf/export/LibreOfficePDFExporter.java
@@ -0,0 +1,107 @@
+package com.pdf.export;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.commons.io.*;
+
+import org.jodconverter.local.*;
+import org.jodconverter.local.office.*;
+
+import org.jodconverter.core.document.*;
+import org.jodconverter.core.office.*;
+
+import com.phill.libs.files.PhillFileUtils;
+
+/** Most reliable class to convert files to PDF format, using a pre-installed LibreOffice.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 1.0, 27/MAR/2023 */
+public class LibreOfficePDFExporter {
+
+    public static void main(String[] args) throws Exception {
+    	
+    	final File inputDir = new File("/Windows/Felipe's Files/Google Drive/Isenções PSI 2023/Rose/Indeferidos");
+    	final File outputDir = new File("/opt/indefs-pdf");
+    	
+    	List<File> filtered = PhillFileUtils.filterByExtension(inputDir, "docx"); Collections.sort(filtered);
+    	
+    	final Map<String, Object> filterData = new HashMap<>();
+    	filterData.put("ExportFormFields", false);
+
+    	final Map<String, Object> customProperties = new HashMap<>();
+    	customProperties.put("Overwrite", true);
+    	customProperties.put("FilterData", filterData);
+    	
+    	int i = 1;
+    	
+    	for (File docx: filtered) {
+    		
+   			final File pdf = new File(outputDir, docx.getName().replaceAll("(?i)docx","pdf"));
+    			
+   			System.out.printf("Processando arquivo %d/%d: '%s'\n", i++, filtered.size(), pdf.getName());
+    			
+   			toPDF(docx, pdf, customProperties);
+    			
+    	}
+    	
+    }
+    
+    /** Converts a stream of bytes to PDF format.
+     *  @param source - source (array of bytes)
+     *  @param format - format of the generated array of bytes
+     *  @param target - target PDF file
+     *  @param options - map of export options
+     *  @throws IOException when the target file cannot be written
+     *  @throws OfficeException when the conversion fails
+     *  @see DefaultDocumentFormatRegistry
+     *  @see <a href="https://github.com/jodconverter/jodconverter/issues/160">JOD Converter Map Specification for options</a>
+     *  @see <a href="https://forum.openoffice.org/en/forum/viewtopic.php?f=44&t=1804&start=0">OpenOffice format specification</a> */
+    public static void toPDF(final byte[] source, final DocumentFormat format, final File target, final Map<String, Object> options) throws IOException, OfficeException {
+    	
+    	final ByteArrayInputStream inputStream = new ByteArrayInputStream(source);
+    	final LocalOfficeManager officeManager = LocalOfficeManager.install(); officeManager.start();
+    	
+    	LocalConverter.builder()
+        	.storeProperties(options)
+        	.officeManager(officeManager)
+        	.build()
+        	.convert(inputStream, true)
+        	.as(format)
+        	.to(target)
+        	.as(DefaultDocumentFormatRegistry.PDF)
+        	.execute();
+    	
+    	OfficeUtils.stopQuietly(officeManager);
+    	
+    }
+    
+    /** Converts a source file do PDF format.<br>
+     *  <b>Note:</b> the format of the source is defined by its file extension!
+     *  @param source - source file
+     *  @param target - target PDF file
+     *  @param options - map of export options
+     *  @throws IOException when the source or target cannot be manipulated
+     *  @throws OfficeException when the conversion fails
+     *  @see DefaultDocumentFormatRegistry
+     *  @see <a href="https://github.com/jodconverter/jodconverter/issues/160">JOD Converter Map Specification for options</a>
+     *  @see <a href="https://forum.openoffice.org/en/forum/viewtopic.php?f=44&t=1804&start=0">OpenOffice format specification</a> */
+    public static void toPDF(final File source, final File target, final Map<String, Object> options) throws IOException, OfficeException {
+    	
+    	final String extension = FilenameUtils.getExtension(source.getName());
+    	final LocalOfficeManager officeManager = LocalOfficeManager.install(); officeManager.start();
+    	
+    	LocalConverter.builder()
+        	.storeProperties(options)
+        	.officeManager(officeManager)
+        	.build()
+        	.convert(source)
+        	.as(DefaultDocumentFormatRegistry.getFormatByExtension(extension))
+        	.to(target)
+        	.as(DefaultDocumentFormatRegistry.PDF)
+        	.execute();
+    	
+    	OfficeUtils.stopQuietly(officeManager);
+    	
+    }
+
+}
---
+++ PDF Exporters/src/com/pdf/export/SimplePDFExporter.java
@@ -0,0 +1,33 @@
+package com.pdf.export;
+
+import java.io.*;
+
+import org.apache.poi.xwpf.usermodel.*;
+
+import fr.opensagres.poi.xwpf.converter.pdf.*;
+
+/** Lightweight class to convert .docx files to PDF format using Apache POI.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 1.0, 27/MAR/2023 */
+public class SimplePDFExporter {
+	
+	/** Converts a .docx file to PDF.
+	 *  @param source - souce .docx file
+	 *  @param target - target PDF file
+	 *  @throws IOException when source or target cannot be manipulated for some reason or source file has other extension than .docx */
+    public static void toPDF(final File source, final File target) throws IOException {
+    	
+    	if (!source.getName().toLowerCase().endsWith(".docx"))
+    		throw new IOException("Source file must be in .docx format!");
+    	
+    	final FileInputStream  inputStream  = new FileInputStream (source);
+    	final FileOutputStream outputStream = new FileOutputStream(target);
+    	
+    	XWPFDocument document = new XWPFDocument(inputStream);
+    	PdfOptions pdfOptions = PdfOptions.create();
+
+    	PdfConverter.getInstance().convert(document, outputStream, pdfOptions);
+    	
+    }
+    
+}
